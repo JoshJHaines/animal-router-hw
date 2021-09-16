9c20d29356f5be6fdd0bc4d2de2c1b58d5906475
JoshuaJHaines <joshjhaines3@gmail.com>
2021-09-16 18:39:51 -0400

    page loads with animal array

--- router/animalRouter.js
+++ router/animalRouter.js
@@ -1,9 +1,17 @@
 const express = require("express")
 const router = express.Router()
 
+let animalArray = [
+    { id: 1, animalName: "dog" },
+    { id: 2, animalName: "cat" },
+    { id: 3, animalName: "hamster" },
+    ];
+    
 
 router.get("/", function(req, res){
-    res.render("index")
+    console.log(animalArray)
+    // res.render("index")
+    res.json({ animalArray })
 })
 
 
